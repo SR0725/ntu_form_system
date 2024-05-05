41d737aa99cfab310a9d8f383e380aa6fc01c395
Ray <62081739+SR0725@users.noreply.github.com>
2024-05-06 04:28:47 +0800

    feat: 登入介面

--- script/login.js
+++ script/login.js
@@ -4,10 +4,34 @@ $(document).ready(function () {
     var username = $("#username").val();
     var password = $("#password").val();
 
-    if (username === "admin" && password === "admin") {
+    // 隱藏所有錯誤信息
+    $(".error-message").hide();
+
+    let hasError = false;
+
+    // 帳號錯誤處理
+    if (username !== "admin") {
+      $("#username")
+        .closest(".form-group")
+        .find(".error-message")
+        .text("帳號錯誤")
+        .show();
+      hasError = true;
+    }
+
+    // 密碼錯誤處理
+    if (password !== "admin") {
+      $("#password")
+        .closest(".form-group")
+        .find(".error-message")
+        .text("密碼錯誤")
+        .show();
+      hasError = true;
+    }
+
+    // 如果沒有錯誤，則轉向指定頁面
+    if (!hasError) {
       window.location.href = "form-list.html";
-    } else {
-      alert("帳號或密碼錯誤");
     }
   });
 });
